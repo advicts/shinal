cc9cbc93a4b21f12ea20e30fd3c1cc3970224112
advicts <softwaresolutions@advicts.com>
2024-03-10 02:40:17 +0300

    Omar Falih

--- intercompany_payment_transfer/models/models.py
+++ intercompany_payment_transfer/models/models.py
@@ -12,30 +12,36 @@ class AccountPayment(models.Model):
     paired_intercompany_payment_id = fields.Many2one('account.payment', 'Paired Intercompany Payment')
 
     def action_post(self):
-        self.move_id._post(soft=False)
+        res=super(AccountPayment, self).action_post()
         if self.is_intercompany_transfer:
             self._create_paired_intercompany_transfer_payment()
-        else:
-            self.filtered(
-                lambda pay: pay.is_internal_transfer or pay.is_intercompany_transfer and not pay.paired_internal_transfer_payment_id
-            )._create_paired_internal_transfer_payment()
+     
 
     def _create_paired_intercompany_transfer_payment(self):
 
         for payment in self:
-            company = self.env.user.company_id
-            if not payment.partner_id.ref_company_ids.journal_code:
-                raise UserError("Please set the journal code for the the company you are using to transfer money.")
+            company = payment.company_id
+            receiver_company = payment.partner_id.ref_company_ids
+
+            if not receiver_company.journal_code:
+                raise UserError("Please set the journal code for the company you are transferring money to.")
+
+            # Get the journal for the receiving company
+            journal = self.env['account.journal'].sudo().search([('company_id', '=', receiver_company.id), ('code', '=', receiver_company.journal_code)], limit=1)
+            if not journal:
+                raise UserError("Journal not found for the receiving company.")
             
-            journal_id = self.env['account.journal'].sudo().search([('company_id', '=', payment.partner_id.ref_company_ids.id), ('code', '=', payment.partner_id.ref_company_ids.journal_code),])
+            amount = payment.amount
+            if payment.currency_id != journal.currency_id:
+                amount = journal.currency_id.sudo()._convert(amount, journal.currency_id, receiver_company.id, payment.date)
 
             paired_payment = self.env['account.payment'].sudo().create({
-                'company_id': payment.partner_id.ref_company_ids.id,
+                'company_id': receiver_company.id,
                 'partner_id': company.partner_id.id,
                 'is_intercompany_transfer': payment.is_intercompany_transfer,
                 'amount': payment.amount,
-                'journal_id': journal_id.id,
-                'currency_id': payment.currency_id.id,
+                'journal_id': journal.id,
+                'currency_id': journal.currency_id.id,
                 'payment_type': payment.payment_type == 'outbound' and 'inbound' or 'outbound',
                 'move_id': None,
                 'ref': payment.ref,
@@ -43,7 +49,6 @@ class AccountPayment(models.Model):
                 'date': payment.date,
             })
 
-            paired_payment.move_id._post(soft=False)
             payment.paired_intercompany_payment_id = paired_payment
 
             body = _('This payment has been created from <a href=# data-oe-model=account.payment data-oe-id=%d>%s</a>') % (payment.id, payment.name)
@@ -51,9 +56,6 @@ class AccountPayment(models.Model):
             body = _('A second payment has been created: <a href=# data-oe-model=account.payment data-oe-id=%d>%s</a>') % (paired_payment.id, paired_payment.name)
             payment.message_post(body=body)
 
-            lines = (payment.move_id.line_ids + paired_payment.move_id.line_ids).filtered(
-                lambda l: l.account_id == payment.destination_account_id and not l.reconciled)
-            lines.reconcile()
 
 
 class ResCompany(models.Model):
