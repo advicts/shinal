c52555d8dc8ad2568b83c43916b6ff476a17b3bd
advicts <softwaresolutions@advicts.com>
2024-03-11 22:08:19 +0300

    Omar Falih

---
+++ print_journal_entries/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Print Journal Entries",
+    'summary': """This module allow user to print journal entry in pdf format.""",
+    'description': """This module allow user to print journal entry in pdf format.""",
+    'author': "Probuse Consulting Service Pvt. Ltd.",
+    'website': "www.probuse.com",
+    'category': 'Account',
+    'version': '0.1',
+    'depends': ['base', 'account'],
+    'data': [
+        'views/report_reg.xml',
+        'views/journal_entires_view.xml',
+    ],
+}
